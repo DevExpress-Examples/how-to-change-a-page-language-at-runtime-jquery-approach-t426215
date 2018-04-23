4adeb259d432c6569c4d76fe9e6bc250c6176876
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ JS/appCode.js
@@ -0,0 +1,38 @@
+﻿function initAPP() {
+    $('#langContainer').dxSelectBox({
+        dataSource: locCode,
+        value: locale,
+        onValueChanged: function (e) {
+            // save the current to a local storage
+            db.load().done(function (data) {
+                if (data.length)
+                    db.update(data[0].lang, { value: e.value });
+                else
+                    db.insert({ value: e.value });
+            });
+            // refresh a page to set a new locale
+            window.location.reload()
+        }
+    });
+
+    $('#btnContainer').dxButton({
+        text: Globalize.formatMessage("btnText"),
+        onClick: function () {
+            DevExpress.ui.dialog.alert(Globalize.formatMessage("alertMessage"), Globalize.formatMessage("alertTitle"));
+        }
+    });
+
+    $('#dateContainer').dxDateBox({
+        value: new Date()
+    });
+
+    $('#gridContainer').dxDataGrid({
+        dataSource: generateData(5),
+        editing: {
+            mode: "row",
+            allowUpdating: true,
+            allowDeleting: true,
+            allowAdding: true
+        }
+    });
+}
---
+++ Readme.md
@@ -0,0 +1,8 @@
+# How to change a page language at runtime (jQuery approach)
+
+
+This example demonstrates how to localize a web page that uses DevExtreme widgets and allows you to change the current language at runtime. We use LocalStore to save/restore the selected language between web site re-execution.<br><br><strong>See also:</strong><br><a href="https://www.devexpress.com/Support/Center/p/T406148">T406148: How to localize DevExtreme widgets using Globalize 1.X when the jQuery approach is used</a>
+
+<br/>
+
+
